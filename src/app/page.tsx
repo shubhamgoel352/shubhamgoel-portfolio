469f98c6b98c0ed4fe84aa95d89a745c6f1f147b
Macbook Pro <macbookpro@MacBook-Pro-2.local>
2025-02-23 21:25:48 -0500

    added new page called ProjectsOrbit.tsx which is Projects preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Hero from "./sections/Hero";
-import Projects from "./sections/Projects";
-import AboutPreview from "./sections/AboutPreview";
 import ProjectsOrbit from "./sections/ProjectsOrbit";
-
+import AboutPreview from "./sections/AboutPreview";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Shubham Goel - AI & Tech Innovator",
   description: "A multi-disciplinary designer and AI strategist.",
 };
